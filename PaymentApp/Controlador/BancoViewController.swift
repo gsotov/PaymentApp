5f979085fc7f832707c4bf3518930debeff6b8f0
Tecnova <tecnova@MacBook-Pro-de-Tecnova.local>
2018-12-21 19:00:21 -0300

    add ajustes

--- PaymentApp/Controlador/BancoViewController.swift
+++ PaymentApp/Controlador/BancoViewController.swift
@@ -8,20 +8,79 @@
 
 import UIKit
 
-class BancoViewController: UIViewController {
+class BancoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
 
+    @IBOutlet weak var tableBanco: UITableView!
     @IBOutlet weak var btnNext: UIButton!
+    @IBOutlet weak var montoLbl: UILabel!
+    @IBOutlet weak var lblTarjeta: UILabel!
+    
+    var arregloBancoApi = [Bancos]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        montoLbl.text = ServiceApi.ParameterValues.cant_pagar
+        lblTarjeta.text = ServiceApi.ParameterValues.nameTC
+        
+        tableBanco.delegate = self
+        tableBanco.dataSource = self
+        
+        getBancosApi()
+        
+        // Do any additional setup after loading the viewtableBanco.
     }
-    
     @IBAction func nextView(_ sender: Any)
     {
         performSegue(withIdentifier: "segue4", sender: nil)
     }
+    
+    func getBancosApi(){
+        ServiceApi.getBancos(TarjetaId: ServiceApi.ParameterValues.tcId){ (success, bancos, error) in
+            if success {
+                if let banco = bancos {
+                    self.arregloBancoApi = banco
+                    self.tableBanco.reloadData()
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arregloBancoApi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellReuseId = "cellTarjeta"
+        let banco = arregloBancoApi[(indexPath as NSIndexPath).row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseId, for: indexPath) as UITableViewCell
+        let imageUrlString = banco.thumbnail
+        let imageUrl = URL(string: imageUrlString ?? "")!
+        let imageData = try! Data(contentsOf: imageUrl)
+        let image = UIImage(data: imageData)
+        cell.imageView?.image = image
+        cell.textLabel?.text = banco.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let banco = arregloBancoApi[(indexPath as NSIndexPath).row]
+        if (banco.name.isEmpty){
+            
+        } else {
+            btnNext.isEnabled = true
+        }
+        
+        ServiceApi.ParameterValues.nameBanco = banco.name
+        ServiceApi.ParameterValues.idBanco = banco.id
+        
+    }
+    
+
     /*
     // MARK: - Navigation
 
--- PaymentApp/Controlador/InstallmentViewController.swift
+++ PaymentApp/Controlador/InstallmentViewController.swift
@@ -10,13 +10,59 @@ import UIKit
 
 class InstallmentViewController: UIViewController {
 
+    @IBOutlet weak var totalPagarLbl: UILabel!
+    @IBOutlet weak var valorCuotaLbl: UILabel!
+    @IBOutlet weak var cuotasLbl: UILabel!
+    @IBOutlet weak var bancoLbl: UILabel!
+    @IBOutlet weak var tarjetaLbl: UILabel!
+    @IBOutlet weak var montoLbl: UILabel!
+    @IBOutlet weak var picketData: UIPickerView!
+    
+    
+    var arregloTotalAPagar: [String] = []
+    var InstallmentsArray = [Int]()
+    var seleccion: Int!
+    var installments: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        totalPagarLbl.text = ServiceApi.ParameterValues.totalPagar
+        tarjetaLbl.text = ServiceApi.ParameterValues.nameTC
+        bancoLbl.text = ServiceApi.ParameterValues.nameBanco
+        
+        getInstallments()
+        
         // Do any additional setup after loading the view.
     }
     
+    func getInstallments(){
 
+        ServiceApi.getMensaje (amount: ServiceApi.ParameterValues.cant_pagar, paymentMethod: ServiceApi.ParameterValues.tcId, issuerId: ServiceApi.ParameterValues.idBanco){ (success, message, error) in
+            if success {
+                
+                let v = Installment.installmentsArray
+                self.installments = v.map { String($0) }
+                self.picketData.reloadAllComponents()
+                self.picketData.selectRow(0, inComponent: 0, animated: false)
+                
+            }
+            
+        }
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.installments.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.installments[row]
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
     /*
     // MARK: - Navigation
 
